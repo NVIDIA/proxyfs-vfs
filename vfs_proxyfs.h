fcab06c8906438481d20bf662de89d7fb7f5271d
balajisrao <balajisrao@yahoo.com>
2017-11-21 15:52:50 -0800

    Readdir byname (#4)
    * Fix readdir to use name instead of offset wherever possible.
    
    * Fix comment in TODO for calling proxyfs_readdir().
    
    * Added marker to telldir() and use name after seekdir() instead of offset.
    
    * Fix comments and align fields in file_handle_s struct.

--- vfs_proxyfs.h
+++ vfs_proxyfs.h
@@ -12,12 +12,21 @@ typedef struct {
 	bool           readonly;
 } fs_ctx_t;
 
+typedef struct telldir_info_s {
+	char	name[NAME_MAX];
+	long	offset;
+	bool	use_name;
+} telldir_info_t;
+
 typedef struct file_handle_s {
-	uint64_t      inum;
-	off_t         offset;
-	uint64_t      flags;
-	uint64_t      mode;
-	struct dirent dir_ent;
+	uint64_t        inum;
+	off_t           offset;
+	uint64_t        flags;
+	uint64_t        mode;
+	struct dirent   dir_ent;
+	bool            use_name;         // Use last returned name as the marker for readdir otherwise offset will be used.
+	char           *prev_readdir_name;
+	telldir_info_t  telldir_info[2];  // 1: Contains the last telldir info and 0: Contains one before that.
 } file_handle_t;
 
 /* fs_ctx_t Operations */
